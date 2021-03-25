d4ec7f4ccd264769b2d2ce3f4af40fe7502998fe
Glitch (hello-express) <none>
2021-03-25 01:15:21 +0000

    🎒📌 Checkpoint ./server.js:14983989/300 ./views/index.html:14983989/515

--- server.js
+++ server.js
@@ -8,31 +8,32 @@ app.use(express.static("public"));
 
 app.get("/", (request, response) => {
   
+  // 1. SET A CSP
   // Very old style: report-uri only
-  
-  // Old style
-    response.set(
-    "Content-Security-Policy",
-    `script-src 'none'; object-src 'none'; report-uri ${REPORTING_ENDPOINT}; report-to csp-endpoint`
-  );
-  // End old style (report-to can be used even in the old style, when paired with the "Report-To" header)
-  
-  // New style
-  // response.set(
+  //   response.set(
   //   "Content-Security-Policy",
-  //   `script-src 'none'; object-src 'none'; report-uri ${REPORTING_ENDPOINT}; report-to csp-endpoint`
+  //   `script-src 'none'; object-src 'none'; report-uri ${REPORTING_ENDPOINT};`
   // );
+
+  // Old style: also with report-to on top of report-uri. report-to can be used even in the old style, when paired with the "Report-To" header.
   
-  // response.set("Reporting-Endpoints", `csp-endpoint="${REPORTING_ENDPOINT}"`);
+  // New style: report-to only (not recommended because of xbrowser support)
+  response.set(
+    "Content-Security-Policy",
+    `script-src 'none'; object-src 'none'; report-to csp-endpoint`
+  );
+  
+  // 2. SET ENDPOINTS
+  response.set("Reporting-Endpoints", `csp-endpoint="${REPORTING_ENDPOINT}"`);
   
   response.sendFile(__dirname + "/views/index.html");
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
+// // send the default array of dreams to the webpage
+// app.get("/dreams", (request, response) => {
+//   // express helps us take JS objects and send them as JSON
+//   response.json(dreams);
+// });
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
