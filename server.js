152a6a478ebdb4602c7ffce6040fd4f832ea0a3a
Glitch (hello-express) <none>
2021-03-25 00:53:13 +0000

    🍰🍶 Checkpoint ./views/index.html:14983989/42 ./server.js:14983989/1777

--- server.js
+++ server.js
@@ -1,38 +1,29 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
 
 const REPORTING_ENDPOINT = "https://csp-reports.glitch.me/reports"
 
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// Reporting-Endpoints: endpoint-1="https://reports.example", endpoint-2="https://reports.example"
-
-// https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
-    response.set("Reporting-Endpoints", `csp-endpoint="${REPORTING_ENDPOINT}"`);
-    // "Reporting-Endpoints: {
+  
+    response.set(
+    "Content-Security-Policy",
+    `script-src 'none'; object-src 'none'; report-uri ${REPORTING_ENDPOINT}; erport-to csp-endpoint`
+  );
+  
+    response.set(
+    // "Report-To": {
     //   group: "csp",
     //   max_age: 10886400,
     //   endpoints: [{ url: "https://csp-reports.glitch.me/post" }]
     // },
-    // "Content-Security-Policy-Report-Only",
-    // `script-src 'none'; object-src 'none'; report-uri ${REPORTING_ENDPOINT}; report-to csp`
-  // );
+    "Content-Security-Policy-Report-Only",
+    `script-src 'none'; object-src 'none'; report-uri ${REPORTING_ENDPOINT}; report-to csp`
+  );
+  
+  response.set("Reporting-Endpoints", `csp-endpoint="${REPORTING_ENDPOINT}"`);
   
   response.sendFile(__dirname + "/views/index.html");
 });
