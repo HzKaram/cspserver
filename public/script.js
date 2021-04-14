a2df6f6d68cc0a4df8ab1c871e3525d3074695c1
Glitch (new-reporting-demo) <none>
2021-04-14 16:58:00 +0000

    🏚🏗 Checkpoint ./public/script.js:14983989/190 ./views/index.html:14983989/10 ./public/style.css:14983989/21

--- public/script.js
+++ public/script.js
@@ -9,8 +9,11 @@ navigator.mediaDevices.getUserMedia({ audio: true }).catch(e => {});
 // Document policy violation
 document.write("<h1>Hey!</h1>");
 
-document.addEventListener('wheel', event => {
-  event.preventDefault();
-});
+// document.addEventListener('wheel', event => {
+//   event.preventDefault();
+// });
+
+var el = document.getElementById("body");
+el.addEventListener("touchstart", () => {console.log("touched")}, false);
 
 performance.measureUserAgentSpecificMemory().catch(e => {});
