35d10a1f2a142939675584e044affa6cff1e2d31
Glitch (new-reporting-demo) <none>
2021-04-15 15:47:46 +0000

    🚨🎒 Checkpoint ./server.js:14983989/2448

--- server.js
+++ server.js
@@ -12,16 +12,25 @@ app.get("/", (request, response) => {
   );
 
   response.set("Permissions-Policy", `microphone=()`);
-  
-  response.set("Document-Policy", `document-write=?0;report-to=main-endpoint`);
-  
-  response.set("Cross-Origin-Embedder-Policy", `require-corp;report-to="main-endpoint"`);
-  
-  response.set("Reporting-Endpoints", `main-endpoint="${REPORTING_ENDPOINT}", default="${REPORTING_ENDPOINT}"`);
 
+  response.set("Document-Policy", `document-write=?0;report-to=main-endpoint`);
+
+  response.set(
+    "Cross-Origin-Embedder-Policy",
+    `require-corp;report-to="main-endpoint"`
+  );
+
+  response.set(
+    "Reporting-Endpoints",
+    `main-endpoint="${REPORTING_ENDPOINT}", default="${REPORTING_ENDPOINT}"`
+  );
+
+  // response.set("Report-To", JSON.stringify({
+  //   max_age: 10886400,
+  //   endpoints: [{ url: `${REPORTING_ENDPOINT}` }]
+  // }));
   response.sendFile(__dirname + "/views/index.html");
 });
-
 
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
