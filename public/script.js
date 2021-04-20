9bf8a0c643682eacceecd8199b0fea867aa50318
Glitch (new-reporting-demo) <none>
2021-04-20 13:38:46 +0000

    🚈👘 Checkpoint ./views/index.html:14983989/18361 ./public/style.css:14983989/630 ./public/script.js:14983989/93

--- public/script.js
+++ public/script.js
@@ -1,10 +1,10 @@
 console.log("hello world :o");
 
-// Deprecation warning
-window.webkitStorageInfo;
-
 // Permissions policy violation
 navigator.mediaDevices.getUserMedia({ audio: true }).catch(e => {});
 
 // Document policy violation
 document.write("<h1>Hey!</h1>");
+
+// Deprecation 
+window.webkitStorageInfo;
