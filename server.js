906dc1a973c3dbc38a618158d3c7f4d775a071dc
Glitch (new-reporting-demo) <none>
2021-04-20 06:55:30 +0000

    🍍🚤 Checkpoint ./server.js:14983989/330

--- server.js
+++ server.js
@@ -5,32 +5,28 @@ const REPORTING_ENDPOINT = "https://reports-endpoint.glitch.me/reports";
 
 app.use(express.static("public"));
 
-app.get("/", (request, response) => {
-  response.set(
-    "Content-Security-Policy",
-    `script-src 'self'; object-src 'none'; report-to main-endpoint;`
-  );
+// app.get("/", (request, response) => {
+//   response.set(
+//     "Content-Security-Policy",
+//     `script-src 'self'; object-src 'none'; report-to main-endpoint;`
+//   );
 
-  response.set("Permissions-Policy", `microphone=()`);
+//   response.set("Permissions-Policy", `microphone=()`);
 
-  response.set("Document-Policy", `document-write=?0;report-to=main-endpoint`);
+//   response.set("Document-Policy", `document-write=?0;report-to=main-endpoint`);
 
-  response.set(
-    "Cross-Origin-Embedder-Policy",
-    `require-corp;report-to="main-endpoint"`
-  );
+//   response.set(
+//     "Cross-Origin-Embedder-Policy",
+//     `require-corp;report-to="main-endpoint"`
+//   );
 
-  response.set(
-    "Reporting-Endpoints",
-    `main-endpoint="${REPORTING_ENDPOINT}", default="${REPORTING_ENDPOINT}"`
-  );
+//   response.set(
+//     "Reporting-Endpoints",
+//     `main-endpoint="${REPORTING_ENDPOINT}", default="${REPORTING_ENDPOINT}"`
+//   );
 
-  // response.set("Report-To", JSON.stringify({
-  //   max_age: 10886400,
-  //   endpoints: [{ url: `${REPORTING_ENDPOINT}` }]
-  // }));
-  response.sendFile(__dirname + "/views/index.html");
-});
+//   response.sendFile(__dirname + "/views/index.html");
+// });
 
 
 app.get("/v0", (request, response) => {
