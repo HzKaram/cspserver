aacfe84216c5f5d3295ccb25a1e3f798ac297a7c
Glitch (new-reporting-demo) <none>
2021-04-20 13:50:14 +0000

    🐗🙋 Checkpoint ./public/script.js:14983989/754 ./views/index.html:14983989/945

--- public/script.js
+++ public/script.js
@@ -1,10 +1,14 @@
 console.log("hello world :o");
 
+// Deprecation
+window.webkitStorageInfo;
+
 // Permissions policy violation
 navigator.mediaDevices.getUserMedia({ audio: true }).catch(e => {});
 
 // Document policy violation
-document.write("<h1>Hey!</h1>");
-
-// Deprecation 
-window.webkitStorageInfo;
+try {
+  document.write("<h1>Hey!</h1>");
+} catch (e) {
+  console.log(e);
+}
