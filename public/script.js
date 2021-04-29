771f41b99625de4d45149c2053a7f7b354836637
Glitch (new-reporting-demo) <none>
2021-04-29 07:58:35 +0000

    👡📒 Checkpoint ./server.js:14983989/1087 ./public/script.js:14983989/714

--- public/script.js
+++ public/script.js
@@ -13,6 +13,25 @@ try {
   console.log(e);
 }
 
+// const hashUrlThisV1 = crypto
+//   .createHash("sha256")
+//   .update("https://new-reporting-api-demo.glitch.me/v1")
+//   .digest("hex");
+// const hashUrlThisV0 = crypto
+//   .createHash("sha256")
+//   .update("https://new-reporting-api-demo.glitch.me/v0")
+//   .digest("hex");
+
+// const hashUrlIntervention = crypto
+//   .createHash("sha256")
+//   .update("https://intervention-generator.glitch.me/")
+//   .digest("hex");
+
+
+// const REPORTS_THIS_V1 = `${REPORTING_ENDPOINT_BASE}?appid=${hashUrlThisV1}`;
+// const REPORTS_THIS_V0 = `${REPORTING_ENDPOINT_BASE}?appid=${hashUrlThisV0}`;
+// const REPORTS_OTHER_APP = `${REPORTING_ENDPOINT_BASE}?appid=${hashUrlIntervention}`;
+// const crypto = require("crypto");
 
 
 
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const crypto = require("crypto");
 
 const REPORTING_ENDPOINT_BASE = "https://reports-endpoint.glitch.me";
 const REPORTS_POST_URL = `${REPORTING_ENDPOINT_BASE}/reports`;
@@ -9,48 +8,34 @@ const INTERVENTION_GENERATOR_URL = "https://intervention-generator.glitch.me/";
 const CODE = "https://glitch.com/edit/#!/new-reporting-api-demo";
 const AUTHOR = "https://twitter.com/maudnals";
 
-// const hashUrlThisV1 = crypto
-//   .createHash("sha256")
-//   .update("https://new-reporting-api-demo.glitch.me/v1")
-//   .digest("hex");
-// const hashUrlThisV0 = crypto
-//   .createHash("sha256")
-//   .update("https://new-reporting-api-demo.glitch.me/v0")
-//   .digest("hex");
-
-// const hashUrlIntervention = crypto
-//   .createHash("sha256")
-//   .update("https://intervention-generator.glitch.me/")
-//   .digest("hex");
-
-
-// const REPORTS_THIS_V1 = `${REPORTING_ENDPOINT_BASE}?appid=${hashUrlThisV1}`;
-// const REPORTS_THIS_V0 = `${REPORTING_ENDPOINT_BASE}?appid=${hashUrlThisV0}`;
-// const REPORTS_OTHER_APP = `${REPORTING_ENDPOINT_BASE}?appid=${hashUrlIntervention}`;
-
 app.use(express.static("public"));
 app.set("view engine", "pug");
 
+app.get("/", (request, response) => {
+  response.redirect("/v1");
+});
+
 app.get("/v1", (request, response) => {
+  
+  // Set the policy and rules that will generate reports when violated
   response.set(
     "Content-Security-Policy",
     `script-src 'self'; object-src 'none'; report-to main-endpoint;`
   );
-
   response.set("Permissions-Policy", `microphone=()`);
-
   response.set("Document-Policy", `document-write=?0;report-to=main-endpoint`);
-
   response.set(
     "Cross-Origin-Embedder-Policy",
     `require-corp;report-to="main-endpoint"`
   );
 
+  // Set the endpoints (API V0)
   response.set(
     "Reporting-Endpoints",
     `main-endpoint="${REPORTS_POST_URL}", default="${REPORTS_POST_URL}"`
   );
 
+  // Send the response
   response.render("index", {
     version: "v1",
     otherVersion: "v0",
@@ -60,33 +45,32 @@ app.get("/v1", (request, response) => {
 });
 
 app.get("/v0", (request, response) => {
+  
+  // Set the policy and rules that will generate reports when violated
   response.set(
     "Content-Security-Policy",
     `script-src 'self'; object-src 'none'; report-to main-endpoint;`
   );
-
   response.set("Permissions-Policy", `microphone=()`);
-
   response.set("Document-Policy", `document-write=?0;report-to=main-endpoint`);
-
   response.set(
     "Cross-Origin-Embedder-Policy",
     `require-corp;report-to="main-endpoint"`
   );
 
+  // Set the endpoints (API V0)
   const mainEndpoint = JSON.stringify({
     group: "main-endpoint",
     max_age: 10886400,
     endpoints: [{ url: `${REPORTS_POST_URL}` }]
   });
-
   const defaultEndpoint = JSON.stringify({
     max_age: 10886400,
     endpoints: [{ url: `${REPORTS_POST_URL}` }]
   });
-
   response.set("Report-To", `${mainEndpoint}, ${defaultEndpoint}`);
 
+  // Send the response
   response.render("index", {
     version: "v0",
     otherVersion: "v1",
