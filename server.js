b615a2cb74b59d1b6cde9b7420f2e928e8d2df70
Glitch (new-reporting-demo) <none>
2021-09-23 14:55:40 +0000

    💾⛲️ Checkpoint ./server.js:14983989/960 ./views/index.pug:14983989/167

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const REPORTING_ENDPOINT_BASE = "https://reports-endpoint.glitch.me";
 const REPORTING_ENDPOINT_MAIN = `${REPORTING_ENDPOINT_BASE}/main`;
 const REPORTING_ENDPOINT_DEFAULT = `${REPORTING_ENDPOINT_BASE}/default`;
 const REPORTS_DISPLAY_URL = REPORTING_ENDPOINT_BASE;
-const INTERVENTION_GENERATOR_URL = "https://intervention-generator.glitch.me/";
+const INTERVENTION_GENERATOR_URL = "https://intervention-report-generator.glitch.me/";
 const CODE_URL = "https://glitch.com/edit/#!/reporting-api-demo";
 const AUTHOR = "https://twitter.com/maudnals";
 const COOP_GENERATOR_URL = "https://coop-report-generator.glitch.me/";
@@ -21,24 +21,14 @@ app.get("/", (request, response) => {
 // - the reporting endpoint for *all* requests
 // - the policy and rules that will generate reports when violated
 app.use(function(request, response, next) {
+
   // Set the endpoints (API V1)
   response.set(
     "Reporting-Endpoints",
     `main-endpoint="${REPORTING_ENDPOINT_MAIN}", default="${REPORTING_ENDPOINT_DEFAULT}"`
   );
 
-  // const mainEndpoint = JSON.stringify({
-  //   group: "main-endpoint",
-  //   max_age: 10886400,
-  //   endpoints: [{ url: `${REPORTING_ENDPOINT_MAIN}` }]
-  // });
-  // const defaultEndpoint = JSON.stringify({
-  //   max_age: 10886400,
-  //   endpoints: [{ url: `${REPORTING_ENDPOINT_DEFAULT}` }]
-  // });
-  // response.set("Report-To", `${mainEndpoint}, ${defaultEndpoint}`);
-
-  // KEEP this ⤵️ with Report-To (v0) IF YOU ALREADY HAVE REPORTING FUNCTIONALITY IN YOUR CODE
+  // API v0 ⤵️ 
   // const mainEndpoint = JSON.stringify({
   //   group: "main-endpoint",
   //   max_age: 10886400,
@@ -51,31 +41,13 @@ app.use(function(request, response, next) {
   // response.set("Report-To", `${mainEndpoint}, ${defaultEndpoint}`);
 
   // Set the rules and policies (these will get violated for the demo)
+  
   response.set(
     "Content-Security-Policy",
     `script-src 'self'; object-src 'none'; report-to main-endpoint;`
   );
+  
   response.set("Document-Policy", `document-write=?0;report-to=main-endpoint`);
-
-  // response.set(
-  //   "Cross-Origin-Opener-Policy",
-  //   `unsafe-none; report-to="main-endpoint"`
-  // );
-
-  // response.set(
-  //   "Cross-Origin-Embedder-Policy",
-  //   `require-corp;report-to="main-endpoint"`
-  // );
-
-  //   response.set(
-  //     "Cross-Origin-Opener-Policy",
-  //     `same-origin; report-to="main-endpoint`
-  //   );
-
-  //   response.set(
-  //     "Cross-Origin-Embedder-Policy",
-  //     `same-origin; report-to="main-endpoint`
-  //   );
 
   // experimental
   response.set("Permissions-Policy", `microphone=()`);
